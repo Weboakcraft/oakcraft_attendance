179e8a55a3f291f28cd03d35a6eff769dcb6e64b
Weboakcraft <mis@oakcraft.in>
2026-08-08 16:36:06 +0530

    Change cache version to v5
    Updated cache version to v5 for service worker.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* OakCraft Attendance - service worker (app shell cache) */
-var CACHE = 'oakcraft-shell-v4';
+var CACHE = 'oakcraft-shell-v5';
 var ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function(e){
